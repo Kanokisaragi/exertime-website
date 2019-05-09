7350873684709d23c28bbf4a8eec227140de7b6c
nhaig <nate.haig@gmail.com>
2019-05-09 12:56:44 +1000

    Add files via upload

--- public_html/include/header.php
+++ public_html/include/header.php
@@ -1,6 +1,8 @@
 <header>
     <nav>
+        
         <ul>
+            <li><img src="./resources/img/head_logo.PNG" alt="Logo"></li>
             <li><a href="home.php" class="current">Home</a></li>
             <li><a href="organisations.php">Organisations</a></li>
             <li><a href="groups.php">Groups</a></li>
